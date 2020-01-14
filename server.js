36dd7d2e4dea054b6b977cfb6cdf265d8b9c4ea1
Juan Millord <jmillord@ambiente.gob.do>
2020-01-14 14:32:13 -0400

    added .env file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const geo = require("./routes/geo.routes");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use("/geo", geo);
 
@@ -11,8 +13,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
+/// DB Connection
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Geo-DB connected!..."));
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  console.log(`The app is listening on port ${port}...`);
 });
